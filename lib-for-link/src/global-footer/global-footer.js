8542a828414d8c701a0b48e92e84ec171e0c1ad4
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-02-28 20:21:57 +0800

    #8 0228 webpage處理中：可以把CSS跟JS整合在一起耶，這也太神了！！

--- lib-for-link/src/global-footer/global-footer.js
+++
@@ -1,4 +0,0 @@
-require('./modules/puliGuestBook/puliGuestBook.js')
-require('./modules/puliGuestBook/init.js')
-require('./modules/puliHandleRecentFunction/puliHandleRecentFunction.js')
-require('./modules/puliHandleRecentFunction/init.js')
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,30 @@ module.exports = {
   entry: {
     'global-footer': [
       './lib-for-link/src/global-footer/modules/puliGuestBook/puliGuestBook.js',
+      './lib-for-link/src/global-footer/modules/puliGuestBook/puliGuestBook.css',
       './lib-for-link/src/global-footer/modules/puliGuestBook/init.js',
       './lib-for-link/src/global-footer/modules/puliHandleRecentFunction/puliHandleRecentFunction.js',
       './lib-for-link/src/global-footer/modules/puliHandleRecentFunction/init.js'
     ],
     'global-header': [
       './lib-for-link/src/global-header/script/google-analytics.js',
-      './lib-for-link/src/global-header/script/script.js'
+      './lib-for-link/src/global-header/script/script.js',
+      './lib-for-link/src/global-header/style/style.css'
     ]
   },
   output: {
     path: path.resolve(__dirname, 'lib-for-link/dist'),
     filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // 針對所有.css 的檔案作預處理，這邊是用 regular express 的格式
+        use: [
+          'style-loader', // 這個會後執行 (順序很重要)
+          'css-loader' // 這個會先執行
+        ]
+      }
+    ]
   }
 }
