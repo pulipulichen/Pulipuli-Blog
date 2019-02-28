c56a037e2b53aa4daa0bfffdc57fa283aea448e9
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-02-28 20:05:14 +0800

    #8 0228 webpage處理中：多重檔案的似乎是可行，那我們來看看css怎麼壓縮

---
+++ lib-for-link/src/global-footer/global-footer.js
@@ -0,0 +1,4 @@
+require('./modules/puliGuestBook/puliGuestBook.js')
+require('./modules/puliGuestBook/init.js')
+require('./modules/puliHandleRecentFunction/puliHandleRecentFunction.js')
+require('./modules/puliHandleRecentFunction/init.js')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'lib-for-link/src/global-header/global-header.js'),
+  entry: {
+    'global-footer': [
+      './lib-for-link/src/global-footer/modules/puliGuestBook/puliGuestBook.js',
+      './lib-for-link/src/global-footer/modules/puliGuestBook/init.js',
+      './lib-for-link/src/global-footer/modules/puliHandleRecentFunction/puliHandleRecentFunction.js',
+      './lib-for-link/src/global-footer/modules/puliHandleRecentFunction/init.js'
+    ],
+    'global-header': [
+      './lib-for-link/src/global-header/script/google-analytics.js',
+      './lib-for-link/src/global-header/script/script.js'
+    ]
+  },
   output: {
-    filename: 'global-header.js',
-    path: path.resolve(__dirname, 'lib-for-link/dist')
+    path: path.resolve(__dirname, 'lib-for-link/dist'),
+    filename: '[name].js'
   }
 }
