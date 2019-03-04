c3ff117286118b04f81c65e6fcdff54c8dba5f18
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-03-05 03:18:19 +0800

    0304 #1 意外的很麻煩，似乎是因為沒辦法抓到bubble

--- lib-for-link/src/page/script/archives.js
+++ lib-for-link/src/page/script/archives.js
@@ -1,14 +1,20 @@
 ArchiveListUtils = {
-  openListYear: function () {
+  openListYear: function (callback) {
     console.log('openListYear')
     //$("#BlogArchive1_ArchiveList > ul.hierarchy > li.archivedate:not(.expanded) a.toggle:eq(0)").trigger("click")
     let li = $("#BlogArchive1_ArchiveList > ul.hierarchy > li.archivedate:not(.expanded)")
     let zippy = $("#BlogArchive1_ArchiveList > ul.hierarchy > li.archivedate:not(.expanded) > a.toggle > .zippy:not(.toggle-open)")
     li.addClass("expanded").removeClass('collapsed')
     zippy.addClass('toggle-open').text('▼ ')
+    
+    if (typeof(callback) === 'function') {
+      callback()
+    }
   },
   openListMonth: function () {
     console.log('openListMonth')
+    
+    let li = $("#BlogArchive1_ArchiveList > ul.hierarchy > li.archivedate > ul.hierarchy > li.archivedate.collapsed")
   },
   displayConfiguration: function () {
     console.log('displayConfiguration')
