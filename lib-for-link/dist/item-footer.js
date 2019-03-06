8baf03adb0193bff144fbe0fe7908f2ce21dd0b0
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-03-06 14:00:23 +0800

    0304 #1 也為此跑遍了整個blog，仔細看一看每一篇有什麼問題也是不錯

--- lib-for-link/dist/item-footer.js
+++ lib-for-link/dist/item-footer.js
@@ -1198,60 +1198,64 @@ if(false) {}
  * 加入圖片的lightbox
  * @author http://lokeshdhakar.com/projects/lightbox2/#getting-started 20160928
  */
-$(function(){
-    if (document.body.clientWidth < 460) {
-        $.each($("article a:has(img)"), function(_i){
-            $(this).attr("target", "_blank");
-        });
-        return;
+$(function () {
+  if (document.body.clientWidth < 460) {
+    $.each($("article a:has(img)"), function (_i) {
+      $(this).attr("target", "_blank");
+    });
+    return;
+  }
+
+  $.each($("article a:has(img)"), function (_i) {
+    var _title = "image";
+    if ($(this).attr("title") !== undefined) {
+      try {
+        _title = $(this).hasAttr("title");
+      } catch (e) {
+
+      }
     }
-    
-    $.each($("article a:has(img)"), function(_i){
-        var _title = "image";
-        if ($(this).attr("title") !== undefined) {
-            _title = $(this).hasAttr("title");
-        }
-        _title = _title + _i;
-        
-        if ($(this).attr("href").indexOf("amazon.com") === -1) {
-            $(this).addClass("lightbox-group")
-                .attr("href", $(this).attr("href").replace(/s1600-h/g, "s1600"))
-                .attr("data-lightbox", _title);
-        }
-    });
-    
-    /*
-    $("head").append($("<link />").attr({
-        rel: "stylesheet",
-        type: "text/css",
-        href: "//pulipulichen.github.io/Pulipuli-Blog/lib-for-link/modules/lightbox2-lokeshdhakar/css/lightbox.min.css"
-        //media: "screen"
-    }));
-    
-    $.getScript("//pulipulichen.github.io/Pulipuli-Blog/lib-for-link/modules/lightbox2-lokeshdhakar/js/lightbox.min.js", function () {
-        lightbox.option({
-            'disableScrolling': true
-        });
-        $("#lightbox .lb-nav").click(function () {
-            var _src = $(this).prev().attr("src");
-            //console.log(_src);
-            if (_src !== undefined) {
-                window.open(_src);
-            }
-        });
-    });
-    */
-    
-    lightbox.option({
-        'disableScrolling': true
-    });
-    $("#lightbox .lb-nav").click(function () {
-        var _src = $(this).prev().attr("src");
-        //console.log(_src);
-        if (_src !== undefined) {
-            window.open(_src);
-        }
-    });
+    _title = _title + _i;
+
+    if ($(this).attr("href").indexOf("amazon.com") === -1) {
+      $(this).addClass("lightbox-group")
+              .attr("href", $(this).attr("href").replace(/s1600-h/g, "s1600"))
+              .attr("data-lightbox", _title);
+    }
+  });
+
+  /*
+   $("head").append($("<link />").attr({
+   rel: "stylesheet",
+   type: "text/css",
+   href: "//pulipulichen.github.io/Pulipuli-Blog/lib-for-link/modules/lightbox2-lokeshdhakar/css/lightbox.min.css"
+   //media: "screen"
+   }));
+   
+   $.getScript("//pulipulichen.github.io/Pulipuli-Blog/lib-for-link/modules/lightbox2-lokeshdhakar/js/lightbox.min.js", function () {
+   lightbox.option({
+   'disableScrolling': true
+   });
+   $("#lightbox .lb-nav").click(function () {
+   var _src = $(this).prev().attr("src");
+   //console.log(_src);
+   if (_src !== undefined) {
+   window.open(_src);
+   }
+   });
+   });
+   */
+
+  lightbox.option({
+    'disableScrolling': true
+  });
+  $("#lightbox .lb-nav").click(function () {
+    var _src = $(this).prev().attr("src");
+    //console.log(_src);
+    if (_src !== undefined) {
+      window.open(_src);
+    }
+  });
 });
 
 /***/ }),
