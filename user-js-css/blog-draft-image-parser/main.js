044767004e77d80fabb4f6d565d995d9c5483c87
Pulipuli Chen <blog@pulipuli.info>
2020-04-08 22:05:07 +0800

    0408 好，這樣就比較好了

--- user-js-css/blog-draft-image-parser/main.js
+++ user-js-css/blog-draft-image-parser/main.js
@@ -1,8 +1,8 @@
 var vm = new Vue({
   el: '#app',
   data: {
-    rawHTML: ''
-    //rawHTML: `<a href="http://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s1600/2020-04-08_202715.png" imageanchor="1"><img border="0" height="240" src="https://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s320/2020-04-08_202715.png" width="320" /></a><a href="http://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s1600/2020-04-08_202611.png" imageanchor="1"><img border="0" height="240" src="https://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s320/2020-04-08_202611.png" width="320" /></a><a href="http://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s1600/2020-04-08_202646.png" imageanchor="1"><img border="0" height="295" src="https://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s320/2020-04-08_202646.png" width="320" /></a>`
+    //rawHTML: ''
+    rawHTML: `<a href="http://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s1600/2020-04-08_202715.png" imageanchor="1"><img border="0" height="240" src="https://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s320/2020-04-08_202715.png" width="320" /></a><a href="http://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s1600/2020-04-08_202611.png" imageanchor="1"><img border="0" height="240" src="https://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s320/2020-04-08_202611.png" width="320" /></a><a href="http://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s1600/2020-04-08_202646.png" imageanchor="1"><img border="0" height="295" src="https://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s320/2020-04-08_202646.png" width="320" /></a>`
   },
   computed: {
     links: function () {
@@ -30,6 +30,9 @@ var vm = new Vue({
       })
       
       return links
+    },
+    outputLinks: function () {
+      return this.links.join('\n\n')
     }
   },
   methods: {
