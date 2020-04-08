f044dd6ca7524c5b7c8f4f9ad77a65f8a6284acf
Pulipuli Chen <blog@pulipuli.info>
2020-04-08 21:54:41 +0800

    0408 改了鏈接呈現的方式，這樣應該比較好上傳嗎...?

--- user-js-css/blog-draft-image-parser/main.js
+++ user-js-css/blog-draft-image-parser/main.js
@@ -1,20 +1,43 @@
 var vm = new Vue({
   el: '#app',
   data: {
-    //rawHTML: ''
-    rawHTML: `<a href="http://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s1600/2020-04-08_202715.png" imageanchor="1"><img border="0" height="240" src="https://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s320/2020-04-08_202715.png" width="320" /></a><a href="http://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s1600/2020-04-08_202611.png" imageanchor="1"><img border="0" height="240" src="https://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s320/2020-04-08_202611.png" width="320" /></a><a href="http://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s1600/2020-04-08_202646.png" imageanchor="1"><img border="0" height="295" src="https://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s320/2020-04-08_202646.png" width="320" /></a>`
+    rawHTML: ''
+    //rawHTML: `<a href="http://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s1600/2020-04-08_202715.png" imageanchor="1"><img border="0" height="240" src="https://4.bp.blogspot.com/-ACQnPYRKEs0/Xo3DijLSUiI/AAAAAAAEjl4/uPoisTw4bZAXzMIL4dwT3swUTPuSsmlGQCK4BGAYYCw/s320/2020-04-08_202715.png" width="320" /></a><a href="http://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s1600/2020-04-08_202611.png" imageanchor="1"><img border="0" height="240" src="https://3.bp.blogspot.com/-U5ZR-dVNmWs/Xo3DizEAWNI/AAAAAAAEjl8/VOiLuZ66MlQuqX0vydj71rtG99FXBQh5ACK4BGAYYCw/s320/2020-04-08_202611.png" width="320" /></a><a href="http://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s1600/2020-04-08_202646.png" imageanchor="1"><img border="0" height="295" src="https://2.bp.blogspot.com/-cujovm3JDjg/Xo3DiV1NkdI/AAAAAAAEjl0/4s__0yA_4ns1LTKaSQQGWFouV6SjHBXIACK4BGAYYCw/s320/2020-04-08_202646.png" width="320" /></a>`
   },
   computed: {
     links: function () {
       let parts = this.rawHTML.split('<a href="')
       //console.log(parts)
       let links = []
+      let filenames = []
+      let filenamesMapping = {}
       for (let i = 1; i < parts.length; i++) {
         let link = parts[i].slice(0, parts[i].indexOf('"'))
-        links.push(link)
+        //links.push(link)
+        let filename = this.filename(link)
+        if (typeof(filenamesMapping[filename]) === 'string') {
+          filename = filename + '_'
+        }
+        
+        filenames.push(filename)
+        filenamesMapping[filename] = link
+        
       }
+      filenames.sort()
+      
+      filenames.forEach(function (filename) {
+        links.push(filenamesMapping[filename])
+      })
       
       return links
+    }
+  },
+  methods: {
+    filename: function (link) {
+      if (link.indexOf('/') === -1) {
+        return link
+      }
+      return link.slice(link.lastIndexOf('/') + 1)
     }
   }
 });
