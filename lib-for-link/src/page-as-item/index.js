80455087991efef7e1ebed1545e6f47f68e1524b
Pulipuli Chen <blog@pulipuli.info>
2021-04-06 21:24:21 +0800

    0406 fixing bugs - #118 Print style

--- lib-for-link/src/page-as-item/index.js
+++ lib-for-link/src/page-as-item/index.js
@@ -1,3 +1,4 @@
 import './../item-footer/admin-tools/AdminToolsStat.js'
 import './../item-footer/admin-tools/AdminToolsStat.less'
 import './../item-footer/script/admin-tools-loader.js'
+import './../item-header/style-print/item-style-print.less'
