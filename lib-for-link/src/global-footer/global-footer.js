319f0c1780a2d79d1de758efb8766945a0a0fe6b
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-07-02 16:36:41 +0800

    西元2024年07月02日 (週二) 16時36分41秒 CST

--- lib-for-link/src/global-footer/global-footer.js
+++ lib-for-link/src/global-footer/global-footer.js
@@ -1,20 +1,20 @@
-import "./puli-guest-book/puli-guest-book.js"
-import "./puli-guest-book/puli-guest-book.less"
-import "./puli-guest-book/init.js"
-import "./blogger-feed-loader/random-posts.js"
-import "./blogger-feed-loader/recent-lib.js"
-import "./blogger-feed-loader/comment-lib.js"
-import "./blogger-feed-loader/init.js"
-import "./cookie-banner/cookie-banner.less"
-import "./cookie-banner/cookie-banner.js"
-import "./blog-statistic/blog-statistic.js"
-import axios from 'axios'
+// import "./puli-guest-book/puli-guest-book.js"
+// import "./puli-guest-book/puli-guest-book.less"
+// import "./puli-guest-book/init.js"
+// import "./blogger-feed-loader/random-posts.js"
+// import "./blogger-feed-loader/recent-lib.js"
+// import "./blogger-feed-loader/comment-lib.js"
+// import "./blogger-feed-loader/init.js"
+// import "./cookie-banner/cookie-banner.less"
+// import "./cookie-banner/cookie-banner.js"
+// import "./blog-statistic/blog-statistic.js"
+// import axios from 'axios'
 
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
-axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
+// axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type';
 
-window.axios = axios
+// window.axios = axios
 
 /**
  * 確認有正常載入用
