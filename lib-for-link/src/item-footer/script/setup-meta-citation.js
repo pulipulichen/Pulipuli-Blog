c2a1483ed60b8c6ac08f4aef32144782acd651bd
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-07-05 20:46:49 +0800

    西元2024年07月05日 (週五) 20時46分49秒 CST

--- lib-for-link/src/item-footer/script/setup-meta-citation.js
+++ lib-for-link/src/item-footer/script/setup-meta-citation.js
@@ -2,17 +2,17 @@ $(() => {
   $('.post-variables:first').each((i, span) => {
     //console.log(i)
     let postVariable = $(span).find('.post-variable')
-    console.log(postVariable)
+    // console.log(postVariable)
 
-    let postTitle = postVariable.attr('data-post-title')
+    let postTitle = postVariable.data('post-title')
     if (postTitle.indexOf(' / ') > -1) {
       postTitle = postTitle.substring(0, postTitle.indexOf(' / ')).trim()
     }
     $(`meata[name="citation_title"]`).attr('content', postTitle)
 
-    $(`meata[name="citation_author"]`).attr('content', postVariable.attr('data-post-author'))
+    $(`meata[name="citation_author"]`).attr('content', postVariable.data('post-author'))
 
-    $(`meata[name="citation_date"]`).attr('content', reformatDate(postVariable.attr('data-post-timestamp')))
+    $(`meata[name="citation_date"]`).attr('content', reformatDate(postVariable.data('post-timestamp')))
 
   })
 })
