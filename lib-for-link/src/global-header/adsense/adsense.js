8a44b2f466f652872f2b9b7c74646a72ec0f39f5
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-05-31 04:16:58 +0800

    西元2025年05月31日 (週六) 04時16分58秒 CST

--- lib-for-link/src/global-header/adsense/adsense.js
+++ lib-for-link/src/global-header/adsense/adsense.js
@@ -10,43 +10,43 @@ function setupAdsense() {
   }
 }
 
-// $(() => {
-//   setTimeout(() => {
-//     let ins = $('ins.adsbygoogle')
-//     ins.addClass('show')
-
-//     let count = ins.length
-//     setTimeout(() => {
-//       for (let i = 0; i < count; i++) {
-//         setupAdsense()
-//       }
-//     }, 500)
-//   }, 3000)
-// })
-
-function loadAdWhenVisible(insSelector) {
-  const ad = document.querySelectorAll(insSelector);
-  if (!ad) return;
-
-  // $(ad).addClass('show')
-  
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      console.log(entry.isIntersecting)
-      if (entry.isIntersecting) {
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        observer.disconnect();
-      }
-    });
-  });
-
-  for (let i = 0; i < ad.length; i++) {
-    observer.observe(ad[i]);
-  }
-}
-
 $(() => {
-  let ins = $('ins.adsbygoogle')
-  ins.addClass('show')
-  loadAdWhenVisible("ins.adsbygoogle");
+  setTimeout(() => {
+    let ins = $('ins.adsbygoogle')
+    ins.addClass('show')
+
+    let count = ins.length
+    setTimeout(() => {
+      for (let i = 0; i < count; i++) {
+        setupAdsense()
+      }
+    }, 500)
+  }, 0)
 })
+
+// function loadAdWhenVisible(insSelector) {
+//   const ad = document.querySelectorAll(insSelector);
+//   if (!ad) return;
+
+//   // $(ad).addClass('show')
+  
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//       console.log(entry.isIntersecting)
+//       if (entry.isIntersecting) {
+//         (adsbygoogle = window.adsbygoogle || []).push({});
+//         observer.disconnect();
+//       }
+//     });
+//   });
+
+//   for (let i = 0; i < ad.length; i++) {
+//     observer.observe(ad[i]);
+//   }
+// }
+
+// $(() => {
+//   let ins = $('ins.adsbygoogle')
+//   ins.addClass('show')
+//   loadAdWhenVisible("ins.adsbygoogle");
+// })
