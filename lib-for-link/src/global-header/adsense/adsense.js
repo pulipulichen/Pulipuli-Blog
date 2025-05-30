a68149c2d3da0a2abd1c532a086e715a4210bf2f
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-05-31 03:58:39 +0800

    西元2025年05月31日 (週六) 03時58分39秒 CST

--- lib-for-link/src/global-header/adsense/adsense.js
+++ lib-for-link/src/global-header/adsense/adsense.js
@@ -4,9 +4,9 @@ function setupAdsense() {
   }
   catch (e) {
     console.error(e)
-    setTimeout(() => {
-      setupAdsense()
-    }, 3000)
+    // setTimeout(() => {
+    //   setupAdsense()
+    // }, 3000)
   }
 }
 
